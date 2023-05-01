8b5490d956c4c511a097506ce462a5e57ffe4618
Andy-Zi <andyziegler@live.de>
2023-05-01 14:14:31 +0200

    remove depreciated library

--- src/__tests__/hooks/useAutocomplete.test.tsx
+++ src/__tests__/hooks/useAutocomplete.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react';
 import useAutocomplete from '../../renderer/hooks/useAutocomplete';
 
 describe('Autocomplete', () => {
